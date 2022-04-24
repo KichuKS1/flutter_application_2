7503d15e484cd807ab923bf7695859e408032b97
KichuKS1 <anugrahks2006@gmail.com>
2022-04-24 13:48:27 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/clock_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ModernClock());
 }
 
 class MyApp extends StatelessWidget {
