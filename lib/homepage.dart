cc684a0fb4e565b6e9e82ec81b0ef56b0e1f639c
KichuKS1 <anugrahks2006@gmail.com>
2022-04-24 20:30:25 +0530

    commit12345

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/clock_view.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -13,27 +13,24 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var now = DateTime.now();
     var formattedTime = DateFormat('HH:mm').format(now);
-    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var formattedDate = DateFormat('EEE, M/d/y').format(now);
     var timezoneString = now.timeZoneOffset.toString().split('.').first;
     var offsetSign = '';
     if (!timezoneString.startsWith('-')) offsetSign = '+';
-    // ignore: avoid_print
     print(timezoneString);
 
     return Scaffold(
-        backgroundColor: const Color(0xFF2D2F41),
-        body: Row(children: <Widget>[
+      backgroundColor: const Color(0xFF2D2F41),
+      body: Row(
+        children: <Widget>[
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               // ignore: deprecated_member_use
-              FlatButton(
-                  onPressed: () {},
-                  child: Column(children: const <Widget>[
-                    FlutterLogo(),
-                    Text('Clock',
-                        style: TextStyle(color: Colors.white, fontSize: 24))
-                  ])),
+              buildMenuButton('Clock', 'assets/clock_icon.png'),
+              buildMenuButton('Alarm', 'assets/alarm_icon.png'),
+              buildMenuButton('Timer', 'assets/timer_icon.png'),
+              buildMenuButton('Stopwatch', 'assets/stopwatch_icon.png'),
             ],
           ),
           const VerticalDivider(
@@ -46,25 +43,67 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  const Text(
-                    'Clock',
-                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: Text(
+                      'Clock',
+                      style: TextStyle(
+                          fontFamily: 'avenir',
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                          fontSize: 24),
+                    ),
                   ),
-                  const SizedBox(height: 32),
-                  Text(
-                    formattedTime,
-                    style: const TextStyle(color: Colors.white, fontSize: 64),
+                  Flexible(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          formattedTime,
+                          style: const TextStyle(
+                              fontFamily: 'avenir',
+                              color: Colors.white,
+                              fontSize: 64),
+                        ),
+                      ],
+                    ),
                   ),
                   Text(
                     formattedDate,
-                    style: const TextStyle(color: Colors.white, fontSize: 20),
+                    style: const TextStyle(
+                        fontFamily: 'avenir',
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontSize: 20),
                   ),
-                  const ClockView(),
-                  const Text(
-                    'Timezone',
-                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  Flexible(
+                      flex: 5,
+                      fit: FlexFit.tight,
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: ClockView(
+                            size: MediaQuery.of(context).size.height / 4,
+                          ))),
+                  Flexible(
+                    flex: 2,
+                    fit: FlexFit.tight,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Timezone',
+                          style: TextStyle(
+                              fontFamily: 'avenir',
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                              fontSize: 24),
+                        ),
+                      ],
+                    ),
                   ),
-                  const SizedBox(
+                  SizedBox(
                     height: 16,
                   ),
                   Row(
@@ -76,8 +115,10 @@ class _HomePageState extends State<HomePage> {
                       const SizedBox(width: 16),
                       Text(
                         'UTC' + offsetSign + timezoneString,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 14),
+                        style: const TextStyle(
+                            fontFamily: 'avenir',
+                            color: Colors.white,
+                            fontSize: 14),
                       ),
                     ],
                   )
@@ -85,9 +126,29 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-        ]));
-  }
+        ],
+      ),
+    );
 
-  // ignore: non_constant_identifier_names
-  DateFormat(String s) {}
+    // ignore: dead_code
+    Padding buildMenuButton(String title, String image) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: TextButton(
+            onPressed: () {},
+            child: Column(children: <Widget>[
+              Image.asset(
+                image,
+                scale: 1.5,
+              ),
+              SizedBox(height: 16),
+              Text(
+                title ?? '',
+                style: TextStyle(
+                    fontFamily: 'avenir', color: Colors.white, fontSize: 14),
+              ),
+            ])),
+      );
+    }
+  }
 }
